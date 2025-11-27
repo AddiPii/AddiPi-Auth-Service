02083fd1a009d56bca6483952906b0e231da4883
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-27 02:58:00 +0100

    chore: add eslint as a dev dependency and improve type annotations in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 //AddiPi-Auth-Service
 import express from 'express'
+import type { Express } from 'express'
 
-const app = express()
+const app: Express = express()
 
 const PORT: string = process.env.AUTH_PORT || "3001"
 
-app.listen(parseInt(PORT), () => {
+app.listen(parseInt(PORT), (): void => {
     console.log(`Addipi Auth Service działa na porcie ${PORT}`)
 })
