647620c30a6503a6dc75ae1e7c90bcbcdbbe3882
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-27 22:27:03 +0100

    feat: enhance user registration validation and error handling

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,31 @@
 import type { Request, Response } from "express"
 import { RegisterReqBody } from "../type"
+import { usersContainer } from "../services/containers"
+import bcrypt from 'bcryptjs'
 
 export const registerUser = async (
     req: Request<{}, unknown, RegisterReqBody>, 
     res: Response
-): Promise<void | {error: string}> => {
+): Promise<void | Response<{error: string}>> => {
     try {
         const { email, password, firstName, lastName } = req.body
         
+        if( !email || !password || !firstName || !lastName ){
+            return res.status(400).json({error: 'All fields are required'})
+        }
+
+        const query: string = `SELECT * FROM c WHERE c.email = @email`
+        const { resources } = await usersContainer.items.query({
+            query,
+            parameters: [{ name: '@email', value: email.toLowerCase() }]
+        }).fetchAll()
+
+        if (resources.length > 0) {
+            return res.status(409).json({error: 'User already exists'})
+        }
+
+        const hashed: string = await bcrypt.hash(password, 10)
+
         
     } catch (err) {
 
