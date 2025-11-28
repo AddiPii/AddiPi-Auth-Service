fe506c9e39bcbea1d0683a12b2e7472340ac27ae
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-28 03:44:22 +0100

    feat: add verify route to authRouter for user verification

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import type { Router } from 'express'
-import { loginUser, logoutUser, refreshToken, registerUser } from '../controllers/authController'
+import { loginUser, logoutUser, refreshToken, registerUser, verifyUser } from '../controllers/authController'
 
 
 export const authRouter: Router = express.Router()
@@ -12,3 +12,5 @@ authRouter.post('/login', loginUser)
 authRouter.post('/refresh', refreshToken)
 
 authRouter.post('/logout', logoutUser)
+
+authRouter.post('/verify', verifyUser)
