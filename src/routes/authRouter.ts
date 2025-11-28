43779be2b2e8bff4f5e625e321886f8b80deab87
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-11-28 02:58:48 +0100

    feat: add login route to authRouter

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import type { Router } from 'express'
-import { registerUser } from '../controllers/authController'
+import { loginUser, registerUser } from '../controllers/authController'
 
 
 export const authRouter: Router = express.Router()
 
 authRouter.post('/register', registerUser)
+
+authRouter.post('/login', loginUser)
